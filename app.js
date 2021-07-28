a7145cb0d28fab3a1eeff603f36131c3da1357e5
Nilesh Gajare <neelesh.gajare@gmail.com>
2021-07-28 08:21:04 +0530

    updated port env

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ mongoose.connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l3fxm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
 ).then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
 }).catch(err => {
     console.log(err);
 });
